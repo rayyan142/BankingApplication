7d04a11e5e2d66f40de2e3b8e77f4e24ca6d6c71
rayyan142 <63351838+rayyan142@users.noreply.github.com>
2025-07-29 14:52:33 -0400

    Delete BankingApplication/BankingApplication directory

--- BankingApplication/BankingApplication/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module coe528.project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens coe528.project to javafx.fxml;
-    exports coe528.project;
-}
